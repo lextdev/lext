cd878824094ea2e3d1afaa2d56681cb4fcdef23d
Yunus SENEM <yunussenem@icloud.com>
2024-01-02 22:54:34 +0300

    Revert AsyncStore

--- package/core/src/types/index.ts
+++ package/core/src/types/index.ts
@@ -4,5 +4,3 @@ export { default as TextSizesProps } from "./TextSizesProps";
 export { default as SpacingProps } from "./SpacingProps";
 export { default as ColorSchemeProps } from "./ColorSchemeProps";
 export { default as BottomSheetRefProps } from "./BottomSheetRefProps";
-export { default as AsyncStorageProps } from "./AsyncStorageProps";
-export { default as MMKVProps } from "./MMKVProps";
