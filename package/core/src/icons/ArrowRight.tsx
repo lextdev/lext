38ccbca8f97a53c38317d810a75a5e8efea4fb59
ynssenem <yunussenem@icloud.com>
2024-01-09 10:44:41 +0300

    🥰 New icons added

--- package/core/src/icons/ArrowRight.tsx
+++ package/core/src/icons/ArrowRight.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Svg, { Path } from "react-native-svg";
+import Svg, { G, Path, Defs, ClipPath } from "react-native-svg";
 import type { SvgProps } from "react-native-svg";
 const SvgArrowRight = (props: SvgProps) => (
   <Svg
@@ -9,10 +9,17 @@ const SvgArrowRight = (props: SvgProps) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <Path
-      fill={props.color || `#FFFFFF`}
-      d="M23.123 9.91 19.252 6a1 1 0 0 0-1.42 0 .998.998 0 0 0 0 1.42l3.56 3.58H1.002a1 1 0 0 0 0 2h20.45l-3.62 3.62a1 1 0 0 0 0 1.38.997.997 0 0 0 1.094.219.998.998 0 0 0 .326-.219l3.87-3.85a3 3 0 0 0 0-4.24"
-    />
+    <G clipPath="url(#a)">
+      <Path
+        fill={props.color || `#FFFFFF`}
+        d="M23.122 9.91 19.252 6a.999.999 0 0 0-1.42 0 1.001 1.001 0 0 0 0 1.42l3.56 3.58H1.002a1 1 0 0 0 0 2h20.45l-3.62 3.62a1 1 0 0 0 0 1.38 1 1 0 0 0 1.42 0l3.87-3.85a3 3 0 0 0 0-4.24"
+      />
+    </G>
+    <Defs>
+      <ClipPath id="a">
+        <Path fill={props.color || `#FFFFFF`} d="M0 0h24v24H0z" />
+      </ClipPath>
+    </Defs>
   </Svg>
 );
 export default SvgArrowRight;
