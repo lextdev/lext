4a62656a69f278f4c9bef83e49fce9f6301cb5d6
ynssenem <yunussenem@icloud.com>
2024-04-08 22:42:48 +0300

    🐛 fix: select updated interface

--- apps/anatolia/components/Main.tsx
+++ apps/anatolia/components/Main.tsx
@@ -1,10 +1,18 @@
-import { Box, Button, ImageBackground, SafeAreaView } from "@lextdev/core"
+import {
+  Box,
+  Button,
+  ImageBackground,
+  SafeAreaView,
+  Select,
+  Text,
+} from "@lextdev/core"
 import React from "react"
 
 const Main = () => {
   return (
     <ImageBackground flex={1}>
       <SafeAreaView>
+        <Select visible={true} header={<Text>Hello World</Text>} data={[]} />
         <Box>
           <Button label="Test" backgroundColor="primary" color="global" />
         </Box>
